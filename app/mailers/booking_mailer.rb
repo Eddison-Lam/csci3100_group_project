0b25f7ea560aa183152775e23c0d94bbde8c3810
Eddison Lam <eddisonlampakyan@gmail.com>
2026-04-13 11:03:26 +0000

    Try to implement sendgrid. Add api and init mailer

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -4,6 +4,9 @@ class BookingMailer < ApplicationMailer
     @booking = booking
     @resource = booking.resource
     @user = booking.user
-    mail(to: @user.email, subject: "Booking Confirmed — #{@resource.name}")
+    time_range = "#{@resource.slot_to_time(@booking.start_slot)} to #{@resource.slot_to_time(@booking.end_slot)}"
+    subject = "[Booking Confirmed] #{@resource.name} - #{@booking.booking_date} #{time_range}"
+    subject = "[DEV] #{subject}" if Rails.env.development?
+    mail(to: @user.email, subject: subject)
   end
 end
